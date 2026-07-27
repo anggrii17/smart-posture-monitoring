13ea093593d8a8dcf5c551095b99def1252a365f
anggrii17 <anggriani22@student.univrab.ac.id>
2026-07-27 09:39:28 +0700

    Menambahkan file untuk deployment Railway

--- api/config.php
+++ api/config.php
@@ -1,11 +1,12 @@
 <?php
 
-$host = "localhost";
-$user = "root";
-$pass = "";
-$db   = "smart_posture";
+$host = getenv("MYSQLHOST");
+$user = getenv("MYSQLUSER");
+$pass = getenv("MYSQLPASSWORD");
+$db   = getenv("MYSQLDATABASE");
+$port = getenv("MYSQLPORT");
 
-$conn = new mysqli($host, $user, $pass, $db);
+$conn = new mysqli($host, $user, $pass, $db, $port);
 
 if ($conn->connect_error) {
     die("Koneksi gagal: " . $conn->connect_error);
---
+++ api/index.php
@@ -0,0 +1,3 @@
+<?php
+
+echo "Smart Posture API is running";

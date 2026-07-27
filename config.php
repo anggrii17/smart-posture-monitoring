6cc7d65db5ac6bb9fa78e07f7b86c55f76abda2b
anggrii17 <anggriani22@student.univrab.ac.id>
2026-07-27 11:46:41 +0700

    fix database config

--- config.php
+++ config.php
@@ -1,17 +1,23 @@
 <?php
 
-$host = getenv("MYSQLHOST");
-$user = getenv("MYSQLUSER");
-$pass = getenv("MYSQLPASSWORD");
-$db   = getenv("MYSQLDATABASE");
-$port = getenv("MYSQLPORT");
+$host = "sakura.proxy.rlwy.net"; // host Railway kamu
+$user = "root";              // username Railway
+$password = "WvMKkCGkCxxfweESECkulizfWbuFkvrB"; // password Railway
+$database = "railway";       // nama database Railway
+$port = 54553;               // port Railway
 
-$conn = new mysqli($host, $user, $pass, $db, $port);
+
+$conn = new mysqli(
+    $host,
+    $user,
+    $password,
+    $database,
+    $port
+);
+
 
 if ($conn->connect_error) {
     die("Koneksi gagal: " . $conn->connect_error);
 }
-
-date_default_timezone_set("Asia/Jakarta");
 
 ?>

db10825f4b639c88d156f3fbb387f921b8707b5d
anggrii17 <anggriani22@student.univrab.ac.id>
2026-07-27 12:59:24 +0700

    fix database config

--- update_current.php
+++ update_current.php
@@ -2,8 +2,20 @@
 
 include "config.php";
 
-$pitch  = $_POST['pitch'];
-$status = $_POST['status'];
+$pitch  = $_POST['pitch'] ?? null;
+$status = $_POST['status'] ?? null;
+
+
+if ($pitch === null || $status === null) {
+
+    echo json_encode([
+        "success"=>false,
+        "message"=>"Data POST kosong"
+    ]);
+
+    exit;
+}
+
 
 $sql = "UPDATE current_posture
         SET
@@ -13,17 +25,23 @@ $sql = "UPDATE current_posture
         last_update=NOW()
         WHERE id=1";
 
+
 if($conn->query($sql)){
+
     echo json_encode([
         "success"=>true,
         "message"=>"Current posture updated"
     ]);
+
 }else{
+
     echo json_encode([
         "success"=>false,
         "message"=>$conn->error
     ]);
+
 }
+
 
 $conn->close();
 
